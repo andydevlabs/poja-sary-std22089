c16d10182a3ef74b257ef85112d719acb963398a
andyDevMdg <andyavotiana@gmail.com>
2024-02-20 22:33:33 +0300

    refactor: format code

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,25 +1,22 @@
 package school.hei.sary.endpoint.rest.controller;
 
-
+import java.io.File;
 import org.springframework.web.bind.annotation.*;
 import school.hei.sary.file.BucketComponent;
 import school.hei.sary.file.FileHash;
 
-import java.io.File;
-
 @RestController
 public class SaryController {
-    BucketComponent bucketComponent;
+  BucketComponent bucketComponent;
 
-    @PutMapping("/sary/{bucketKey}")
-    public FileHash saryUpload(
-            @RequestParam(value="file") File file,
-            @PathVariable String bucketKey){
-        return bucketComponent.upload(file, bucketKey);
-    }
+  @PutMapping("/sary/{bucketKey}")
+  public FileHash saryUpload(
+      @RequestParam(value = "file") File file, @PathVariable String bucketKey) {
+    return bucketComponent.upload(file, bucketKey);
+  }
 
-    @GetMapping("/sary/black/{bucketKey}")
-    public File saryDownload(@PathVariable String bucketKey){
-        return bucketComponent.download(bucketKey);
-    }
+  @GetMapping("/sary/black/{bucketKey}")
+  public File saryDownload(@PathVariable String bucketKey) {
+    return bucketComponent.download(bucketKey);
+  }
 }
