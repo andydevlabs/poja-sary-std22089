04981fe11ff1d9f83716f607e4df1e99112d6ad5
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 08:44:00 +0300

    feat: upload and download controller

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,22 +1,40 @@
 package school.hei.sary.endpoint.rest.controller;
 
 import java.io.File;
+import java.net.URL;
+import java.time.Duration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import school.hei.sary.file.BucketComponent;
 import school.hei.sary.file.FileHash;
 
 @RestController
 public class SaryController {
-  BucketComponent bucketComponent;
+  private final BucketComponent bucketComponent;
 
-  @PutMapping("/sary/{bucketKey}")
-  public FileHash saryUpload(
-      @RequestParam(value = "file") File file, @PathVariable String bucketKey) {
-    return bucketComponent.upload(file, bucketKey);
+    public SaryController(BucketComponent bucketComponent) {
+        this.bucketComponent = bucketComponent;
+    }
+
+    @PutMapping("/sary/")
+  public ResponseEntity<?> uploadFile(@RequestParam("file") File file, @RequestParam("bucketKey") String bucketKey) {
+    try {
+      FileHash fileHash = bucketComponent.upload(file, bucketKey);
+      return ResponseEntity.ok(fileHash);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
   }
 
-  @GetMapping("/sary/black/{bucketKey}")
-  public File saryDownload(@PathVariable String bucketKey) {
-    return bucketComponent.download(bucketKey);
+  @GetMapping("/sary/{bucketKey}")
+  public ResponseEntity<?> downloadFile(@PathVariable String bucketKey) {
+    try {
+      URL presignedUrl = bucketComponent.presign(bucketKey, Duration.ofMinutes(10)); // Adjust expiration as needed
+      return ResponseEntity.ok(presignedUrl);
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
   }
 }
