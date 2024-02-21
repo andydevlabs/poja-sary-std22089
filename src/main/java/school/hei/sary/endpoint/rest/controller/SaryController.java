8554ceca57b3248ade8295cd0a109e849005a18e
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 10:52:27 +0300

    SaryController

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,12 +1,14 @@
 package school.hei.sary.endpoint.rest.controller;
 
 import java.io.File;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.sary.file.BucketComponent;
 
+@AllArgsConstructor
 @RestController
 public class SaryController {
   BucketComponent bucketComponent;
