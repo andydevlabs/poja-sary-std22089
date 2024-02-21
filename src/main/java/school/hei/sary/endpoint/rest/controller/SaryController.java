dded06f6a1416dd31ddaf0a398ac73b6aa3efb4b
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 10:45:36 +0300

    feat: format code SaryController

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,23 +1,21 @@
 package school.hei.sary.endpoint.rest.controller;
 
+import java.io.File;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.hei.sary.file.BucketComponent;
 
-import java.io.File;
-
 @RestController
 public class SaryController {
-    BucketComponent bucketComponent;
+  BucketComponent bucketComponent;
 
-    @PutMapping("/blacks/")
-    public ResponseEntity<String> uploadSary(
-            @RequestParam(value = "file") File file,
-            @RequestParam(value = "bucketKey") String bucketKey){
-        bucketComponent.upload(file, bucketKey);
-        return ResponseEntity.ok("Success");
-    }
-
+  @PutMapping("/blacks/")
+  public ResponseEntity<String> uploadSary(
+      @RequestParam(value = "file") File file,
+      @RequestParam(value = "bucketKey") String bucketKey) {
+    bucketComponent.upload(file, bucketKey);
+    return ResponseEntity.ok("Success");
+  }
 }
