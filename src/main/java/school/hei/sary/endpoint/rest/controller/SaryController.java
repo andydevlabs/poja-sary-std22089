bfba37d11e9881356d027b4a4b83932e50240129
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 12:38:39 +0300

    feat: upload controller

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,11 +1,22 @@
 package school.hei.sary.endpoint.rest.controller;
 
+import static java.util.UUID.randomUUID;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.time.Duration;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import school.hei.sary.file.BucketComponent;
 
 @AllArgsConstructor
@@ -13,11 +24,34 @@ import school.hei.sary.file.BucketComponent;
 public class SaryController {
   BucketComponent bucketComponent;
 
-  @PutMapping("/blacks/")
-  public ResponseEntity<String> uploadSary(
-      @RequestParam(value = "file") File file,
-      @RequestParam(value = "bucketKey") String bucketKey) {
-    bucketComponent.upload(file, bucketKey);
-    return ResponseEntity.ok("Success");
+  private static final String SARY_KEY = "sary/";
+
+  @PutMapping("/sary/{bucketKey}")
+  public ResponseEntity<String> uploadAndSignImage(
+      @RequestParam("file") MultipartFile multipartFile,
+      @PathVariable String bucketKey,
+      @RequestParam("expirationMinutes") Long expirationMinutes) {
+    try {
+      // Convert MultipartFile to File
+      File file = convertMultipartFileToFile(multipartFile);
+
+      // Upload file to S3
+      String imageBucketKey = SARY_KEY + bucketKey;
+      bucketComponent.upload(file, imageBucketKey);
+
+      // Get pre-signed URL
+      URL preSignedUrl =
+          bucketComponent.presign(imageBucketKey, Duration.ofMinutes(expirationMinutes));
+
+      return ResponseEntity.of(Optional.of(preSignedUrl.toString()));
+    } catch (Exception e) {
+      return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
+    }
+  }
+
+  private File convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+    Path tempFile = Files.createTempFile(randomUUID().toString(), ".jpg");
+    Files.copy(multipartFile.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
+    return tempFile.toFile();
   }
 }
