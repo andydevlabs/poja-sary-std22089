6d8dd5af01f0bab67e95fcd2c27dc5a246903fce
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 08:53:08 +0300

    feat: upload and download controller

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -3,7 +3,6 @@ package school.hei.sary.endpoint.rest.controller;
 import java.io.File;
 import java.net.URL;
 import java.time.Duration;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +13,13 @@ import school.hei.sary.file.FileHash;
 public class SaryController {
   private final BucketComponent bucketComponent;
 
-    public SaryController(BucketComponent bucketComponent) {
-        this.bucketComponent = bucketComponent;
-    }
+  public SaryController(BucketComponent bucketComponent) {
+    this.bucketComponent = bucketComponent;
+  }
 
-    @PutMapping("/sary/")
-  public ResponseEntity<?> uploadFile(@RequestParam("file") File file, @RequestParam("bucketKey") String bucketKey) {
+  @PutMapping("/sary/")
+  public ResponseEntity<?> uploadFile(
+      @RequestParam("file") File file, @RequestParam("bucketKey") String bucketKey) {
     try {
       FileHash fileHash = bucketComponent.upload(file, bucketKey);
       return ResponseEntity.ok(fileHash);
@@ -31,7 +31,8 @@ public class SaryController {
   @GetMapping("/sary/{bucketKey}")
   public ResponseEntity<?> downloadFile(@PathVariable String bucketKey) {
     try {
-      URL presignedUrl = bucketComponent.presign(bucketKey, Duration.ofMinutes(10)); // Adjust expiration as needed
+      URL presignedUrl =
+          bucketComponent.presign(bucketKey, Duration.ofMinutes(10)); // Adjust expiration as needed
       return ResponseEntity.ok(presignedUrl);
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
