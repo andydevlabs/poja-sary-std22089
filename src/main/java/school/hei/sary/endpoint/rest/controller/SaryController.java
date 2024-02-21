d98efb3ded5524d026b16bb81f6f6677cda93198
andyDevMdg <andyavotiana@gmail.com>
2024-02-21 12:48:04 +0300

    feat: reupload controller

--- src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/SaryController.java
@@ -1,57 +1,81 @@
 package school.hei.sary.endpoint.rest.controller;
 
+import static java.io.File.createTempFile;
+import static java.nio.file.Files.createTempDirectory;
 import static java.util.UUID.randomUUID;
+import static school.hei.sary.file.FileHashAlgorithm.NONE;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 import school.hei.sary.file.BucketComponent;
+import school.hei.sary.file.FileHash;
 
 @AllArgsConstructor
 @RestController
 public class SaryController {
+
   BucketComponent bucketComponent;
 
   private static final String SARY_KEY = "sary/";
 
-  @PutMapping("/sary/{bucketKey}")
-  public ResponseEntity<String> uploadAndSignImage(
-      @RequestParam("file") MultipartFile multipartFile,
-      @PathVariable String bucketKey,
-      @RequestParam("expirationMinutes") Long expirationMinutes) {
-    try {
-      // Convert MultipartFile to File
-      File file = convertMultipartFileToFile(multipartFile);
+  @GetMapping(value = "/sary/bucket")
+  public ResponseEntity<String> file_can_be_uploaded_then_signed() throws IOException {
+    var fileSuffix = ".txt";
+    var filePrefix = randomUUID().toString();
+    var fileToUpload = createTempFile(filePrefix, fileSuffix);
+    writeRandomContent(fileToUpload);
+    var fileBucketKey = SARY_KEY + filePrefix + fileSuffix;
+    can_upload_file_then_download_file(fileToUpload, fileBucketKey);
 
-      // Upload file to S3
-      String imageBucketKey = SARY_KEY + bucketKey;
-      bucketComponent.upload(file, imageBucketKey);
+    var directoryPrefix = "dir-" + randomUUID();
+    var directoryToUpload = createTempDirectory(directoryPrefix).toFile();
+    var fileInDirectory =
+        new File(directoryToUpload.getAbsolutePath() + "/" + randomUUID() + ".txt");
+    writeRandomContent(fileInDirectory);
+    var directoryBucketKey = SARY_KEY + directoryPrefix;
+    can_upload_directory(directoryToUpload, directoryBucketKey);
 
-      // Get pre-signed URL
-      URL preSignedUrl =
-          bucketComponent.presign(imageBucketKey, Duration.ofMinutes(expirationMinutes));
-
-      return ResponseEntity.of(Optional.of(preSignedUrl.toString()));
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body("Error uploading file: " + e.getMessage());
-    }
+    return ResponseEntity.of(Optional.of(can_presign(fileBucketKey).toString()));
   }
 
-  private File convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
-    Path tempFile = Files.createTempFile(randomUUID().toString(), ".jpg");
-    Files.copy(multipartFile.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
-    return tempFile.toFile();
+  private void writeRandomContent(File file) throws IOException {
+    FileWriter writer = new FileWriter(file);
+    var content = randomUUID().toString();
+    writer.write(content);
+    writer.close();
+  }
+
+  private File can_upload_file_then_download_file(File toUpload, String bucketKey)
+      throws IOException {
+    bucketComponent.upload(toUpload, bucketKey);
+
+    var downloaded = bucketComponent.download(bucketKey);
+    var downloadedContent = Files.readString(downloaded.toPath());
+    var uploadedContent = Files.readString(toUpload.toPath());
+    if (!uploadedContent.equals(downloadedContent)) {
+      throw new RuntimeException("Uploaded and downloaded contents mismatch");
+    }
+
+    return downloaded;
+  }
+
+  private FileHash can_upload_directory(File toUpload, String bucketKey) {
+    var hash = bucketComponent.upload(toUpload, bucketKey);
+    if (!NONE.equals(hash.algorithm())) {
+      throw new RuntimeException("FileHashAlgorithm.NONE expected but got: " + hash.algorithm());
+    }
+    return hash;
+  }
+
+  private URL can_presign(String fileBucketKey) {
+    return bucketComponent.presign(fileBucketKey, Duration.ofMinutes(2));
   }
 }
